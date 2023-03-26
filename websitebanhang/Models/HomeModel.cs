6e56b646362c3e4772bc1c5a39ea2911b01cce58
baukien97 <you@example.com>
2023-03-26 19:31:49 +0700

    up code

---
+++ websitebanhang/Models/HomeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using websitebanhang.Context;
+namespace websitebanhang.Models
+{
+    public class HomeModel
+    {
+        public List<LoaiSanPham> ListLoaiSanPham { get; set; }
+        public List<SanPham> ListSanPham { get; set; }
+    }
+}
